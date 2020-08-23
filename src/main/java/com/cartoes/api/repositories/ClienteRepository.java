be12313c606e97d056da02804ca3246ce7b83cad
Marcus Guimarães <marcusv308@icloud.com>
2020-08-23 01:20:24 -0300

    Camada Repository

--- src/main/java/com/cartoes/api/repositories/ClienteRepository.java
+++ src/main/java/com/cartoes/api/repositories/ClienteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import com.cartoes.api.entities.Cliente;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
  @Transactional(readOnly = true)
  Cliente findByCpf(String cpf);
---
+++ src/main/java/com/cartoes/api/repositories/TransacaoRepository.java
@@ -0,0 +1,22 @@
+package com.cartoes.api.repositories;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.cartoes.api.entities.Transacao;
+
+@Transactional(readOnly = true)
+public interface TransacaoRepository  extends JpaRepository<Transacao, Integer> {
+
+
+@Query("SELECT t FROM Transacao t WHERE t.cartao.id = :cartaoId")
+List<Transacao> findByCartaoId(@Param("cartaoId") int cartaoId);
+
+}

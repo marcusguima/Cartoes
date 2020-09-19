320b7081bb31c51ced1e689a266e8099c6b847ac
Marcus Guimarães <marcusv308@icloud.com>
2020-09-19 05:47:58 -0300

    Criação dos Métodos bloquearUsuario e atualizarDataAcesso na UsuarioRepository

--- src/main/java/com/cartoes/api/repositories/UsuarioRepository.java
+++ src/main/java/com/cartoes/api/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.cartoes.api.repositories;
  
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,5 +19,25 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
    	@Modifying(clearAutomatically = true)
    	@Query("UPDATE Usuario SET senha = :novasenha WHERE id = :idusuario")
    	void alterarSenhaUsuario(@Param("novasenha") String novasenha, @Param("idusuario") int idusuario);
+   	
+  	@Transactional
+  	@Query("UPDATE Usuario SET ativo = FALSE WHERE DATEDIFF(now(), :dataAcesso) > 30 AND id = :idusuario")
+  	void bloquearUsuario(@Param("dataAcesso") Date dataAcesso, @Param("idusuario")int idusuario);
+  	
+   	@Transactional
+   	@Query("UPDATE Usuario SET dataAcesso = now() WHERE id = :idusuario")
+   	void atualizarDataAcesso(@Param("idusuario")int idusuario);
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
+   	
    	
 }
